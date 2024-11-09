f3a0908dc12cb9df61417adc0d0bc440da5f3ca4
YJB-Gay <129620824+YJB-Gay@users.noreply.github.com>
2024-11-09 18:12:30 -0500

    Update index.php
    Implement Clean URLs and POST Method

--- index.php
+++ index.php
@@ -7,76 +7,79 @@ use Endroid\QrCode\QrCode;
 function smallHash($text)
 {
     $t = rtrim(base64_encode(hash('crc32', $text, true)), '=');
-
     return strtr($t, '+/', '-_');
 }
 
-if (!empty($_GET['url'])) {
-    $url = $_GET['url'];
+$content = '';
 
+// Check if the request method is POST and if the URL is provided
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['url'])) {
+    $url = $_POST['url'];
+
+    // Ensure the URL starts with http:// or https://
     if (strpos($url, 'http://') !== 0 && strpos($url, 'https://') !== 0) {
         $url = 'http://'.$url;
     }
 
     $urlhash = smallHash($url);
-
     $hashfolder = substr($urlhash, 0, 2);
     $hashfile = substr($urlhash, 2);
 
     $hashfolderpath = './db/'.$hashfolder;
     $hashfilepath = $hashfolderpath.'/'.$hashfile;
 
-    mkdir($hashfolderpath, 0700, true);
+    // Create the directory if it doesn't exist
+    if (!is_dir($hashfolderpath)) {
+        mkdir($hashfolderpath, 0700, true);
+    }
 
+    // Save the URL to a file
     file_put_contents($hashfilepath, $url);
 
-    $shortUrl = $_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'].'?'.$urlhash;
+    // Generate the short URL without exposing index.php
+    $shortUrl = 'https://' . $_SERVER['HTTP_HOST'] . '/' . $urlhash;
 
-    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
-        $shortUrl = 'https://'.$shortUrl;
-    } else {
-        $shortUrl = 'http://'.$shortUrl;
-    }
-
+    // Generate the QR code for the short URL
     $qrcode = (new QrCode())->setText($shortUrl);
 
+    // Prepare the content to display the short URL and QR code
     $content = '<a href="'.$shortUrl.'">'.$shortUrl.'</a><br>'
-	    .'<img src="data:'.$qrcode->getContentType().';base64,'.base64_encode($qrcode->get()).'">';
+        .'<img src="data:'.$qrcode->getContentType().';base64,'.base64_encode($qrcode->get()).'">';
 } elseif (!empty($_GET)) {
+    // Handle the case where a hash is provided in the URL
     $urlhash = key($_GET);
-
     $hashfolder = substr($urlhash, 0, 2);
     $hashfile = substr($urlhash, 2);
 
     $hashfolderpath = './db/'.$hashfolder;
     $hashfilepath = $hashfolderpath.'/'.$hashfile;
 
+    // Check if the file exists
     $findfiles = glob($hashfilepath);
-
     if (!empty($findfiles)) {
         $fullfilepath = current($findfiles);
-
         header('Location:'.file_get_contents($fullfilepath));
-        return 0;
+        exit;
     }
 
-    $content = 'No link match this identifier.';
+    $content = 'No link matches this identifier.';
 } else {
-    $content = '<form method="get">
-				Enter your URL: <input type="text" name="url"><input type="submit" value="Submit">
-			</form>';
+    // Show the form for URL input
+    $content = '<form method="post">
+                Enter your URL: <input type="text" name="url"><input type="submit" value="Submit">
+            </form>';
 }
 
-//actual page below
+// Actual page output
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Shuri</title>
-	</head>
-	<body>
-		<div id="content">
-			<?= $content ?>
-		</div>
-	</body>
+<head>
+    <title>Shuri</title>
+</head>
+<body>
+    <div id="content">
+        <?= $content ?>
+    </div>
+</body>
 </html>
